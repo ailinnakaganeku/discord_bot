dcdf0ecf6a31d9cb9027f5074192fca3a22888b9
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-03-24 08:09:05 -0300

    first commit boti

---
+++ index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+);
